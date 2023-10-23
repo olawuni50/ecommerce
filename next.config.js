278f8ac48823338e4d8ebe2b8d7fde86db7ca364
Olawuni Olukunle <olawuni50@gmail.com>
2023-10-23 21:57:15 +0200

    next config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,10 @@ const nextConfig = {
         domains: ['yt3.ggpht.com',
       'lh3.googleusercontent.com',
       "cdn.sanity.io", "images.unsplash.com", "plus.unsplash.com"]
-      }
+      },
+      typescript:{
+        ignoreBuildErrors: true,
+    }
 }
 
 module.exports = nextConfig
